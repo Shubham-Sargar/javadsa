7230d34a4e5e798db677dedd29a3cc4f66661877
Shubham Sargar <sargarshubham69@gmail.com>
2026-07-05 19:39:34 +0530

    reverse the array using temp pointer array #GFG

---
+++ ArrayReverse.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+public class ArrayReverse {
+    public static void reverseArray(int []arr){
+          int n=arr.length;
+        int temp[]= new int[n];
+        for(int i=0;i<n;i++){
+             temp[i] = arr[n-i-1];
+        }
+        for(int i=0;i<n;i++){
+            arr[i]= temp[i];
+        }
+    }
+    public static void main(String[]args){
+        int arr[]= {5,4,3,2,1,0};
+        reverseArray(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+}
